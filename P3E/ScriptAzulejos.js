c67a8b534405bb238bb003043a2d501bdfbc817e
Joelitr0 <cisnerosalexis027@gmail.com>
2025-10-01 22:57:17 -0700

    Programacion Examen

---
+++ P3E/ScriptAzulejos.js
@@ -0,0 +1,22 @@
+function calcular() {
+  var largo = parseFloat(document.getElementById("largo").value);
+  var ancho = parseFloat(document.getElementById("ancho").value);
+  var precio = parseFloat(document.getElementById("precio").value);
+
+  var areaSuperficie = (largo * 100) * (ancho * 100);
+
+  var areaAzulejo = 25 * 25;
+
+  var cantidadAzulejos = (areaSuperficie / areaAzulejo);
+
+  var precioTotal = cantidadAzulejos * precio;
+
+  var resultado = document.getElementById("resultado");
+  resultado.style.display = "block";
+  resultado.innerHTML = `
+    <p><strong>Area de la superficie:</strong> ${areaSuperficie.toLocaleString()} cm2</p>
+    <p><strong>Area de un azulejo:</strong> ${areaAzulejo} cm2</p>
+    <p><strong>Cantidad de azulejos:</strong> ${cantidadAzulejos}</p>
+    <p><strong>Precio total:</strong> $${precioTotal.toFixed(2)}</p>
+  `;
+}
